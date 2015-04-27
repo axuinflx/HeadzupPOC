851431f6f83f2a10f4ffa158718ca1d1c2115bdf
axuinflx <Allen@ITs-MacBook-Pro-2.local>
2015-04-27 18:29:06 -0400

    Support local notification

--- HeadzupPOC/ViewController.swift
+++ HeadzupPOC/ViewController.swift
@@ -13,6 +13,15 @@ import UIKit
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // local notification
+        var locNotification: UILocalNotification = UILocalNotification()
+        locNotification.alertAction = "Check Daily Challenge"
+        locNotification.alertBody = "Check your daily challenge"
+        locNotification.fireDate = NSDate(timeIntervalSinceNow: 30)
+        //locNotification.
+        UIApplication.sharedApplication().scheduleLocalNotification(locNotification)
+        
     }
 
     override func didReceiveMemoryWarning() {
