ade3e76239791af7dd60468776bc46df47d16af9
ahworeta <dev@Sandeep-MAC.local>
2015-04-28 20:55:24 -0400

    Integrated login UI with the backend service

--- HeadzupPOC/ViewController.swift
+++ HeadzupPOC/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 @IBDesignable class ViewController: UIViewController {
 
+    @IBOutlet weak var phoneNumberTF: UITextField!
+    @IBOutlet weak var pinTF: UITextField!
+    
+    @IBOutlet weak var feedbackLB: UILabel!
+    var theConnnection: NSURLConnection?
+    
+    var webServiceData:NSMutableData?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -30,10 +38,72 @@ import UIKit
     }
 
     @IBAction func LoginButtonPressed(sender: UIButton) {
+       
+        let userPhoneNumber = phoneNumberTF.text
+        let userPassword = pinTF.text
         
-        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("MainNavigation") as? UIViewController
         
-        self.presentViewController(vc!, animated: true, completion: nil)
+        
+        if(userPhoneNumber.isEmpty || userPassword.isEmpty)
+        {
+            displayAlertMessage("All fields are required.")
+            return
+        }
+        
+        let parametersString = "email=\(userPhoneNumber)&password=\(userPassword)"
+        var url:String = "http://10.200.20.86/api/mobileservice/Login?\(parametersString)"
+        
+        let theURL = NSURL(string: url)!
+        var req:NSURLRequest = NSURLRequest(URL: theURL)
+        
+        theConnnection = NSURLConnection(request: req, delegate: self, startImmediately: true)
+        
+        
+        
+        
+    }
+    func connection(connection: NSURLConnection, didReceiveResponse response: NSURLResponse)
+    {
+        webServiceData = NSMutableData()
+        
+    }
+    func connection(connection: NSURLConnection, didReceiveData data: NSData)
+    {
+        webServiceData?.appendData(data)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection) {
+        
+        let JsonStr = NSString(data: webServiceData!, encoding: NSUTF8StringEncoding)
+        
+        println("json = \(JsonStr)")
+        
+        var jsonDict =  NSJSONSerialization.JSONObjectWithData(webServiceData!, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
+        
+        println("jsonDict = \(jsonDict)")
+        
+        var status = jsonDict["Status"] as! NSInteger!
+        
+        if(status == 1)
+            
+        {
+            
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("MainNavigation") as? UIViewController
+            
+            self.presentViewController(vc!, animated: true, completion: nil)
+        }
+        else
+        {
+            feedbackLB.text = jsonDict["Message"] as! String!
+            
+        }
+        
+    }
+
+    
+    func connection(connection: NSURLConnection, didFailWithError error: NSError) {
+        
+        println("connection failed \(error.description))")
         
     }
     
@@ -41,5 +111,12 @@ import UIKit
         self.view.endEditing(true)
     }
 
+    func displayAlertMessage(userMessage:String)
+    {
+        var myAlert = UIAlertController(title: "Message", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil)
+        myAlert.addAction(okAction)
+        self.presentViewController(myAlert, animated: true, completion: nil)
+    }
 }
 
