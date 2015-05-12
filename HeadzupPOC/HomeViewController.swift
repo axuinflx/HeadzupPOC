c13de4a923d07d453e5e0c3c581a0dac67c08ac1
Matt Solano <msolano@inflexxion.com>
2015-05-12 11:12:55 -0400

    Some more testing

--- HeadzupPOC/HomeViewController.swift
+++ HeadzupPOC/HomeViewController.swift
@@ -13,16 +13,28 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var FaceOuterView: UIView!
     
+    let facesArray = ["Face1", "Face2", "Face3"]
+//    let swipeGestureRecognizer = UISwipeGestureRecognizer()
+    var currentIndex = 0
+    
+    
+    
     struct Face {
         let faceView: FaceView
     }
-    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var firstFace = createFace("Face1")
+        
+        
+        var currentFace = facesArray[currentIndex]
+        var firstFace = createFace(currentFace)
+        
+        //swipeGestureRecognizer.addTarget(self, action: "swipeAction:")
+        //firstFace.faceView.addGestureRecognizer(swipeGestureRecognizer)
+        
         FaceOuterView.addSubview(firstFace.faceView)
         
     }
@@ -38,6 +50,43 @@ class HomeViewController: UIViewController {
         faceView.image = image
         return Face(faceView:faceView)
     }
+
+    
+    @IBAction func SwipedLeft(sender: UISwipeGestureRecognizer) {
+        println("swiped")
+        
+        for view in FaceOuterView.subviews as! [UIView] {
+            
+            println(FaceOuterView.subviews.count)
+            
+            var viewDesc = view.description
+            println("\(viewDesc)")
+            
+            
+            
+            UIView.animateWithDuration(1.4, animations: {
+                view.transform = CGAffineTransformMakeTranslation(-214, 0)
+            }, completion: { finished in
+                view.removeFromSuperview()
+                var newFace = self.createFace("Face2")
+                newFace.faceView.frame.origin.x = 214
+                self.FaceOuterView.addSubview(newFace.faceView)
+                
+                UIView.animateWithDuration(0.4, animations: {
+                   newFace.faceView.transform = CGAffineTransformMakeTranslation(-214, 0)
+                }, completion: nil)
+                
+                
+            })
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
     
 
     /*
